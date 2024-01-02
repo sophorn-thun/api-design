c6dea612b9d350c9eca0d158e9a7d02b37d950e6
sophorn thun <thunsophorn012@gmail.com>
2024-01-02 14:49:58 +0100

    Adding createUser and Signin handler functions

---
+++ src/handlers/user.ts
@@ -0,0 +1,34 @@
+import prisma from "../db";
+import { comparePasswords, createJWT, hashPassword } from "../modules/auth";
+
+export const createUser = async (req, res) => {
+    const user = await prisma.user.create({
+        data: {
+            username: req.body.username,
+            password: await hashPassword(req.body.password)
+        }
+    })
+
+    const token = createJWT(user)
+    res.json({ token })
+}
+
+export const signin = async (req, res) => {
+    const user = await prisma.user.findUnique({
+        where: {
+            username: req.body.username
+        }
+    })
+
+    const isValid = await comparePasswords(req.body.password, user.password)
+
+    if (!isValid) {
+        res.status(401)
+        res.json({message: 'not valid'})
+        return
+    }
+
+    const token = createJWT(user)
+    res.json({ token })
+
+}
