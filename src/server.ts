75f0660c9181132e09bba90f2b8f02677f18a234
sophorn thun <thunsophorn012@gmail.com>
2024-01-02 10:19:58 +0100

    Adding protect middleware

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,22 @@
 import express from'express';
+import router from './router';
+import { protect } from './modules/auth';
+import cors from 'cors';
+import morgan from 'morgan'
 
-export const app = express();
+const app = express();
+
+app.use(cors());
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
 app.get('/', (req, res) => {
     console.log('hello')
     res.status(200);
     res.json({message: 'hello'})
 })
+
+app.use('/api', protect, router)
 
 export default app;
