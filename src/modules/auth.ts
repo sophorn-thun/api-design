1af80479b1f93d43a7ab84eb4e684e3caf1250a1
sophorn thun <thunsophorn012@gmail.com>
2024-01-02 10:42:56 +0100

    Updating protect middleware

--- src/modules/auth.ts
+++ src/modules/auth.ts
@@ -16,4 +16,22 @@ export const protect = (req, res, next) => {
         res.json({message: 'not authorized'})
         return
     }
+
+    const [, token] = bearer.split(' ')
+
+    if (!token) {
+        res.status(401)
+        res.json({message: 'not valid token'})
+        return
+    }
+
+    try {
+        const user = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = user
+        next()
+    } catch (e) {
+        res.status(401)
+        res.json({message: 'not valid token'})
+        return
+    }
 }
