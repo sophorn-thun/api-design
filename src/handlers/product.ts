e9b553bb64eeffb1cf5686d627b4e49cbb342530
sophorn thun <thunsophorn012@gmail.com>
2024-01-03 15:22:12 +0100

    Adding get product handlers

---
+++ src/handlers/product.ts
@@ -0,0 +1,27 @@
+import prisma from "../db"
+
+// Get all products
+export const getProducts = async (req, res) => {
+    const user = await prisma.user.findUnique({
+        where: {
+            id: req.user.id
+        },
+        include: {
+            products: true
+    }
+})
+
+    res.json({ data: user.products})
+}
+
+// Get one product
+export const getOneProduct = async (req, res) => {
+    const id = req.params.id
+    const product = await prisma.product.findFirst({
+        where: {
+            id,
+            belongsToId: req.user.id
+        }
+    })
+    res.json({data: product})
+}
