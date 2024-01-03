951c560b0042e9f036f9165c1d2a978d604e0227
sophorn thun <thunsophorn012@gmail.com>
2024-01-03 11:33:20 +0100

    adding validation middleware to routes

--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,10 @@
 import {Router} from 'express'
-import { body } from "express-validator";
+import { body, oneOf } from "express-validator";
 import { handleInputError } from './modules/middleware';
 
 const router = Router()
 
+// Product
 router.get('/product', (req, res) => {
     res.json({message: 'product'})
 })
@@ -14,14 +15,32 @@ router.put('/product/:id', body('name').isString(), handleInputError, (req, res)
 })
 router.delete('/product/:id', () => {})
 
+// Update
 router.get('/update', () => {})
-router.post('/update', () => {})
-router.put('/update/:id', () => {})
+router.post('/update', 
+    body('title').exists().isString(),
+    body('body').exists().isString(),
+    body('productId').exists().isString(),
+    () => {})
+router.put('/update/:id', 
+    body('title').optional(),
+    body('body').optional(),
+    body('status').isIn(['IN_PROGRESS', 'SHIPPED', 'DEPRECATED']).optional(),
+    body('version').optional(), 
+    () => {})
 router.delete('/update/:id', () => {})
 
+// Updatepoint
 router.get('/updatepoint', () => {})
-router.post('/updatepoint', () => {})
-router.put('/updatepoint/:id', () => {})
+router.post('/updatepoint', 
+    body('name').isString(), 
+    body('description').isString(),
+    body('updateId').exists().isString(),
+    () => {})
+router.put('/updatepoint/:id', 
+    body('name').optional().isString(), 
+    body('description').optional().isString(),
+    () => {})
 router.delete('/updatepoint/:id', () => {})
 
 export default router;
