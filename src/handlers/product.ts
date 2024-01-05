8e7844163e9ea05992f86545466d4284bd6d2179
sophorn thun <thunsophorn012@gmail.com>
2024-01-05 14:48:07 +0100

    Adding environment config

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -27,14 +27,17 @@ export const getOneProduct = async (req, res) => {
 }
 
 // Create product
-export const createProduct = async (req, res) => {
-    const product = await prisma.product.create({
+export const createProduct = async (req, res, next) => {
+    try {const product = await prisma.product.create({
         data: {
             name: req.body.name,
             belongsToId: req.user.id
         }
     })
     res.json({data: product})
+    } catch (e) {
+        next(e)
+    }
 }
 
 // Update Product
